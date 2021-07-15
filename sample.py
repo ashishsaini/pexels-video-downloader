d87c1714074e2a6af59c130ff4cf12f0819306c4
Ashy saini <ashy@Ashys-MacBook-Air.local>
2021-07-15 14:04:33 +0530

    log parameter on initialisation on class

--- sample.py
+++ sample.py
@@ -9,9 +9,8 @@ from pexels_downloader import PexelsDownloader
 if __name__ == "__main__":
 
     pexels_url = "https://www.pexels.com/video/waves-rushing-and-splashing-to-the-shore-1409899/"
-    pexels_d = PexelsDownloader()
+    pexels_d = PexelsDownloader('logs/pexels_downloader.log')
     pexels_d.downloads_dir = "downloads/"
     pexels_d.resolution = 1080
-    pexels_d.log_path = 'logs/pexels_downloader.log'
     result = pexels_d.download(pexels_url)
     print(result)
