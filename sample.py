8e3ebb890f9c92d1680732cb454c754ed3645249
ashishsaini <ashy.saini@gmail.com>
2021-10-02 21:47:56 +0530

    added example for image search

--- sample.py
+++ sample.py
@@ -4,24 +4,21 @@ This script downloads the video from pexels when pexels video URL is passed
 Usage: python main.py "{pexels_url}"
 '''
 
-from pexels_downloader import PexelsDownloader
+from video_downloader import VideoDownloader
+from image_downloader import ImageDownloader
 
 if __name__ == "__main__":
 
-    # downlaod video
-    # pexels_url = "https://www.pexels.com/video/waves-rushing-and-splashing-to-the-shore-1409899/"
-    # pexels_d = PexelsDownloader('logs/pexels_downloader.log')
-    # video_id = pexels_d.get_id_from_url(pexels_url)
-    # pexels_d.downloads_dir = "downloads/"
-    # pexels_d.resolution = 1080
-    # result = pexels_d.download(video_id)
-    # print(result)
-
-    # exit()
-
     # search video
-    pexels_d = PexelsDownloader('logs/pexels_downloader.log')
-    pexels_d.downloads_dir = "downloads/"
-    pexels_d.resolution = 1080
-    result = pexels_d.search_video("forest")
+    video = VideoDownloader('logs/pexels_downloader.log')
+    video.downloads_dir = "downloads/"
+    video.resolution_width = 1080
+    result = video.search_video("forest")
+    print(result)
+
+    # image search 
+    image = ImageDownloader('logs/pexels_downloader.log')
+    image.resolution_width = 1280
+    image.resolution_height = 720
+    result = image.get_image("black")
     print(result)
